40afa1b9e6c09a9e5d4050dd5572dccfe1725d15
Vedant Mehta <vedantmehta@vedant.local>
2024-09-15 22:24:51 +0530

    swap seats of two consecutive students

--- query_5.sql
+++ query_5.sql
@@ -15,3 +15,21 @@
 -- que: swap seats of 2 consecutive students
 -- link: https://www.youtube.com/watch?v=Sq1kM3jVU68&list=PLBTZqjSKn0IfuIqbMIqzS-waofsPHMS0E&index=28
 
+select * from seats;
+
+-- 1. using case when 
+
+select id,student,
+case when id = (select max(id) from seats) and id%2=1 then id
+when id%2=1 then id+1 else id-1 end as new_seat
+from seats;
+
+-- 2 . using lead and lag
+select id,student,
+case 
+when id%2=1 then lead(id,1,id) over(order by id) 
+else lag(id,1,id) over(order by id) end as new_id
+from seats;
+
+
+
