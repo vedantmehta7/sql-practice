820f78b849b9c0c98e630df91dd212feaa34a815
Vedant Mehta <vedantmehta@vedant.local>
2024-09-15 23:15:35 +0530

    origin destination total tickets

---
+++ query_7.sql
@@ -0,0 +1,23 @@
+-- CREATE TABLE air_tickets (
+--     airline_number VARCHAR(10),
+--     origin VARCHAR(3),
+--     destination VARCHAR(3),
+--     oneway_round CHAR(1),
+--     ticket_count INT
+-- );
+
+
+-- INSERT INTO air_tickets (airline_number, origin, destination, oneway_round, ticket_count)
+-- VALUES
+--     ('DEF456', 'BOM', 'DEL', 'O', 150),
+--     ('GHI789', 'DEL', 'BOM', 'R', 50),
+--     ('JKL012', 'BOM', 'DEL', 'R', 75),
+--     ('MNO345', 'DEL', 'NYC', 'O', 200),
+--     ('PQR678', 'NYC', 'DEL', 'O', 180),
+--     ('STU901', 'NYC', 'DEL', 'R', 60),
+--     ('ABC123', 'DEL', 'BOM', 'O', 100),
+--     ('VWX234', 'DEL', 'NYC', 'R', 90);
+
+-- que: 
+-- link: https://www.youtube.com/watch?v=rGG6FAYHDNc&list=PLBTZqjSKn0IfuIqbMIqzS-waofsPHMS0E&index=24
+
