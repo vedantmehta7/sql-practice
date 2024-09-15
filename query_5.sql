fde02957b6aa444ac1f4629f6fe08d762edd4c53
Vedant Mehta <vedantmehta@vedant.local>
2024-09-15 22:15:57 +0530

    swap seats of two consecutive students

---
+++ query_5.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE seats (
+--     id INT,
+--     student VARCHAR(10)
+-- );
+
+-- INSERT INTO seats VALUES 
+-- (1, 'Amit'),
+-- (2, 'Deepa'),
+-- (3, 'Rohit'),
+-- (4, 'Anjali'),
+-- (5, 'Neha'),
+-- (6, 'Sanjay'),
+-- (7, 'Priya');
+
+-- que: swap seats of 2 consecutive students
+-- link: https://www.youtube.com/watch?v=Sq1kM3jVU68&list=PLBTZqjSKn0IfuIqbMIqzS-waofsPHMS0E&index=28
+
